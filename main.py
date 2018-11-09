c8d619460e800325ebdebf19c9523fe9a53910d8
zhzhang <zhzhang2015@sina.com>
2018-11-09 19:24:49 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+################################################################################
+#   File Name  : main.py
+##  Author     : zhzhang
+### E-mail     : zhzhang2015@sina.com
+### Created on : Fri 09 Nov 2018 07:24:03 PM CET
+##  Version    : unknown
+#   License    : MIT
+################################################################################
+
