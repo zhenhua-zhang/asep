424c0b27c8fa66a216340c41179d57cf3a66a66a
zhenhua-zhang <zhenhua.zhang217@gmail.com>
2019-01-13 22:27:08 +0100

    update adjusment_of_reference_bias.py

--- predictors/adjustment_of_reference_bias.py
+++ predictors/adjustment_of_reference_bias.py
@@ -1,8 +1,10 @@
 
 import gzip
 import pysam
-import bcftools
 
+from operator import itemgetter
+# from operator import attrgetter
+# from operator import methodcaller
 
 """
 
@@ -10,7 +12,7 @@ import bcftools
 awk '{if ($1 ~ /^[1-9XY]+){print $0}} Homo_sapiens.GRCh37.71.gtf \
     > Homo_sapiens.GRCh37.71.Chr1-22.X.Y.gtf
 
-# Sort the gtf on by chrommosome(column 1) and coordination(column 4 and 5), 
+# Sort the gtf on by chrommosome(column 1) and coordination(column 4 and 5),
 # then compress it by bgzip
 cat Homo_sapiens.GRCh37.71.Chr1-22.X.Y.gtf \
     | sort -k1,1d -k4,4n -k5,5n \
@@ -25,10 +27,10 @@ tabix -p gff Homo_sapiens.GRCh37.71.Chr1-22.X.Y.gtf.bgz
 class BiasAdjustment:
     """Adjust reference bias
 
-    When mapping reads to reference genome(e.g. GRCh37), there is a bias that 
-reference alleles have more chance be mapped and consequently the expect ratio of
-alternative-counts / reference-counts is less than 1;
-"""
+    When mapping reads to reference genome(e.g. GRCh37), there is a bias that
+    reference alleles have more chance be mapped and consequently the expect ratio of
+    alternative-counts / reference-counts is less than 1;
+    """
 
     def __init__(self, fn):
         """Initialization of BiasAdjustment
@@ -39,8 +41,16 @@ alternative-counts / reference-counts is less than 1;
         """__str__() method; return specific strings"""
         return "BiasAdjustment"
 
+    def read_file(self, fn, mode='r'):
+        """Load the target file"""
+
+        if fn.endswith('.gz') or fn.endswith('.bgz'):
+            return gzip.open(fn, mode=mode)
+        else:
+            return open(fn, mode=mode)
+
     @staticmethod
-    def is_indexed(self, file_name, suffix, check_time_stamp=False):
+    def is_indexed(file_name, suffix, check_time_stamp=False):
         """Check if the input file has updated index file"""
         older = None
         if check_time_stamp:
@@ -51,21 +61,42 @@ alternative-counts / reference-counts is less than 1;
         indexed = os.path.exists(file_name)
         return (indexed, older)
 
-    def read_file(self, fn):
-        """Load the target file"""
-        mode = 'r'
-        openF = open
+    def sort_tsv(self, file_name, with_patch=False, file_type=None,
+                 chrom_col=None, pos_col=None):
+        """Sort tab-separated file
+        """
+        if file_type in ['vcf', 'bed']:
+            chrom_col = 0
+            pos_col = 1
+        elif file_type in ['gff', 'gtf', 'gff3']:
+            chrom_col = 0
+            pos_col = 3
+        elif None not in [file_type, chrom_col, pos_col]:
+            pass
+        else:
+            raise ValueError(
+                '''Please leave file_type and specify chrom_col and pos_col.'''
+            )
 
-        if fn.endswith('.gz') or fn.endswith('.bgz'):
-            openF = gzip.open
-            mode = 'rb'
+        header, lines, name_column = [], [], ''
+        file_handler = self.read_file(file_name, mode='r')
 
-        with openF(fn, mode=mode) as fnh:
-            return fnh
+        line = next(file_handler, 'EOF')
+        assert line is not 'EOF', 'The file cannot be empty.'
+
+        # {lambda x: len(x) for x in list(file_handler) if x.startswith('#')}
+
+        while line != 'EOF':
+            if line.startswith('#'):
+                header.append(line)
+            line = next(file_handler, 'EOF')
+
+        lines = [line.split('\t') for x in list(file_handler)]
+        lines = sorted(lines, key=itemgetter(chrom_col, pos_col))
+        return iter(header.extend(lines))
 
     def make_index(self, file_name, file_type='fa'):
         """Make index file for input file"""
-
         if file_type == 'fa':
             self.is_indexed(file_name, '.fai')
             pysam.faidx(file_name)
@@ -92,7 +123,6 @@ alternative-counts / reference-counts is less than 1;
 
     def overall_p(self):
         """Expectation of global probability of reads including alternative allele"""
-
         gene_list = []
         region = self.get_gene_retion(genes=gene_list)
         gene_region = "{chrom}:{start}-{end}".format(**region)
@@ -108,4 +138,4 @@ alternative-counts / reference-counts is less than 1;
 
 
 ipf = '../misc/chr22.fa'
-adj = BiasAdjustment(ipf)
+# adj = BiasAdjustment(ipf)
