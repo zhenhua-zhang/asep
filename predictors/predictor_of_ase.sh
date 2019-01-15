4a75d858f025b12f7a1792e617e061c80b7ef06b
zhenhua-zhang <zhenhua.zhang217@gmail.com>
2019-01-15 16:19:50 +0100

    add sbatch script for predictor_of_ase.py

---
+++ predictors/predictor_of_ase.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# File Name  : predictor_of_ase.sh
+# Author     : zhzhang
+# E-mail     : zhzhang2015@sina.com
+# Created on : Tue 15 Jan 2019 04:13:01 PM CET
+# Version    : v0.0.1
+# License    : MIT
+#
+#SBATCH --time=1:0:0
+#SBATCH --output=%j-%u-predictor_of_ase.log
+#SBATCH --job-name=predictor_of_ase
+#SBATCH --ntasks=1
+#SBATCH --cpus_per_task=1
+#SBATCH --mem_per_cpu=1G
+
+module list 
+pwd
+
+ASED=Documents/projects/ASEpredictor
+source ${HOME}/${ASED}/env/bin/activite
+python ${HOME}/${ASED}/outputs/predictor/predictor_of_ase.py
+
+if [ $? -eq 0 ]; then
+	echo "Job was done!"
+else
+	echo "Job returned without 0"
+fi
