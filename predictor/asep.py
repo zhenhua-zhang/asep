31e17068a22ddd7530c1115444489a9e69a28fa2
zhenhua-zhang <zhenhua.zhang217@gmail.com>
2019-03-13 11:39:45 +0100

    update asep.py

--- predictor/asep.py
+++ predictor/asep.py
@@ -7,17 +7,18 @@ import os
 import sys
 from argparse import ArgumentParser
 from asep.predictor import ASEPredictor
+from asep.configs import Config
 
 
 def get_args():
     """A method to get arguments from the command line"""
 
     __head = """
-        ##############################################\n
-        #    Allele-specific expression predictor    #\n
-        #                                            #\n
-        #  Zhenhua Zhang <zhenhua.zhang217@gmai.com  #\n
-        ##############################################\n
+        ##############################################
+        #    Allele-specific expression predictor    #
+        #                                            #
+        # Zhenhua Zhang <zhenhua.zhang217@gmail.com> #
+        ##############################################
     """
 
     __foot = """
@@ -42,38 +43,58 @@ def get_args():
         help="Only read first k rows as input from input file"
     )
     group.add_argument(
-        "-m", "--mask", dest="mask", default=None,
+        "-m", "--mask", dest="mask", default=None, type=str,
         help="Pattern will be masked or kept"
     )
     group.add_argument(
-        "-s", "--group-size", dest="group_size", default=5, type=int,
-        help="The least number of individuals bearing the same variant"
+        "--min-group-size", dest="min_group_size", default=2, type=int,
+        help="The minimum number of individuals bearing the same variant (>=2)"
     )
     group.add_argument(
-        "-S", "--skip-column", dest="skip_columns", default=None,
-        help="""The columns will be skipped. Seperated by semi-colon and quote
+        "--max-group-size", dest="max_group_size", default=None, type=int,
+        help="The maximum number of individuals bearing the same variant"
+    )
+    group.add_argument(
+        "--drop-cols", dest="drop_cols", default=None,
+        help="""The columns will be dropped. Seperated by semi-colon and quote
         them by ','. if there are more than one columns."""
     )
     group.add_argument(
-        "-t", "--target-col", dest="target_col", default=None,
+        "--response-col", dest="reponse_col", default='bb_ASE', # XXX: Default must bu consistent with input dataset
         help="The column name of response variable or target variable"
     )
 
     group = parser.add_argument_group("Output")
     group.add_argument(
-        "-o", "--output-dir", dest="output_dir", default='./',
+        "-o", "--output-dir", dest="output_dir", default='./', type=str,
         help="The directory including output files. Default: ./"
     )
 
     group = parser.add_argument_group("Configuration")
     group.add_argument(
-        "-c", "--config-file", dest="config_file", default=None,
+        "-c", "--config-file", dest="config_file", default=None, type=str,
         help="""The path to configuration file, all configuration will be get
         from it, and overwrite values from command line except -i"""
     )
     group.add_argument(
-        "-C", "--cross_validations", dest="cross_validations", default=8,
-        type=int, help="How many folds of cross-validation will be done"
+        "--inner-cvs", dest="inner_cvs", default=6, type=int, 
+        help="Fold of cross-validations for RandomizedSearchCV"
+    )
+    group.add_argument(
+        "--inner-n-jobs", dest="inner_n_jobs", default=5, type=int,
+        help="Number of jobs for RandomizedSearchCV"
+    )
+    group.add_argument(
+        "--inner-n-iters", dest="inner_n_iters", default=20, type=int,
+        help="Number of iters for RandomizedSearchCV"
+    )
+    group.add_argument(
+        "--outer-cvs", dest="outer_cvs", default=6, type=int, 
+        help="Fold of cross-validation for outer_validation"
+    )
+    group.add_argument(
+        "--outer-n-jobs", dest="outer_n_jobs", default=5, type=int,
+        help="Number of jobs for outer_validation"
     )
 
     group = parser.add_argument_group("Misc")
@@ -82,7 +103,7 @@ def get_args():
         help="the proportion of dataset for testing"
     )
     group.add_argument(
-        "--run-flag", dest="run_flag", default=None,
+        "--run-flag", dest="run_flag", default="New task",
         help="Flags for current run"
     )
 
@@ -95,44 +116,68 @@ def main():
     arguments = parser.parse_args()
 
     # config_file = arguments.config_file
-    cross_validations = arguments.cross_validations
-    # first_k_rows = arguments.first_k_rows
-    group_size = arguments.group_size
-    input_file = arguments.input_file
-    # mask = arguments.mask
-    output_dir = arguments.output_dir
-    # run_flag = arguments.run_flag
-    # skip_columns = arguments.skip_columns
-    # target_col = arguments.target_col
     # test_size = arguments.test_size
     # validation_file = arguments.validation_file
 
     # /home/umcg-zzhang/Documents/projects/ASEPrediction/training/outputs/biosGavinOverlapCov10/biosGavinOlCv10AntUfltCstBin.tsv
 
-    asep = ASEPredictor(input_file)
+    # HEAD::Config for model training
+    my_config = Config()
+    inner_cvs = arguments.inner_cvs
+    if inner_cvs != 6:
+        my_config.optim_params['cv'] = StratifiedKFold(
+            n_splits=inner_cvs, shuffle=True
+        )
+    else:
+        pass
 
-    mask = 'group_size < {}'.format(group_size)
+    inner_n_jobs = arguments.inner_n_jobs
+    if inner_n_jobs != 5:
+        my_config.optim_params['n_jobs'] = inner_n_jobs
+    else:
+        pass
 
-    # Use Beta-Binomial
-    print("\First run on bb_ASE")
-    response = 'bb_ASE' # target_col
-    trim = [
-        "log2FC", "bn_p", "bn_p_adj", "bb_p", "bb_p_adj", "group_size",
-        "bn_ASE", "CaddChrom", "CaddPos", "CaddRef", "CaddAlt", "GeneName",
-        "motifEName", "GeneID", "FeatureID", "GeneName", "targetScan"
-    ]
-    asep.run(mask=mask, trim_cols=trim, response=response, cvs_=cross_validations)
-    asep.save_to(output_dir)
+    inner_n_iters = arguments.inner_n_iters
+    if inner_n_iters != 20:
+        my_config.optim_params['n_iter'] = inner_n_iters
+    else:
+        pass
+    # TAIL::Config for model training
 
-    print("\nAnother run on bn_ASE")
-    # Use Bionmial
-    response = 'bn_ASE'
-    trim = [
-        "log2FC", "bn_p", "bn_p_adj", "bb_p", "bb_p_adj", "group_size",
-        "bb_ASE", "CaddChrom", "CaddPos", "CaddRef", "CaddAlt", "GeneName",
-        "motifEName", "GeneID", "FeatureID", "GeneName", "targetScan"
-    ]
-    asep.run(mask=mask, trim_cols=trim, response=response, cvs_=cross_validations)
+    input_file = arguments.input_file
+    asep = ASEPredictor(input_file, my_config)
+
+    run_flag = arguments.run_flag
+    print("{:-^80}".format(run_flag))
+
+    drop_cols = arguments.drop_cols
+    if drop_cols is None:
+        drop_cols = [
+            "log2FC", "bn_p", "bn_p_adj", "bb_p", "bb_p_adj", "group_size",
+            "bn_ASE", "CaddChrom", "CaddPos", "CaddRef", "CaddAlt", "GeneName",
+            "GeneID", "FeatureID", "GeneName"
+        ]
+    else:
+        pass
+
+    min_group_size = arguments.min_group_size
+    if min_group_size < 2:
+        raise("--min-group-size must be at least 2")
+    else:
+        pass
+
+    max_group_size = arguments.max_group_size
+
+    first_k_rows = arguments.first_k_rows
+    reponse_col = arguments.reponse_col
+    outer_cvs = arguments.outer_cvs
+    mask = arguments.mask
+    asep.run(
+        mask=mask, cvs_=outer_cvs, mings=min_group_size, maxgs=max_group_size,
+        limit=first_k_rows, response=reponse_col, drop_cols=drop_cols,
+    )
+
+    output_dir = arguments.output_dir
     asep.save_to(output_dir)
 
 if __name__ == '__main__':
