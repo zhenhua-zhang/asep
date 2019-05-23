2961d879c5e310751967cab22f2d3b195a450e5d
zhenhua-zhang <zhenhua.zhang217@gmail.com>
2019-05-23 16:57:00 +0200

    update

--- discover/transform_expression_abundance_into_binary_v0.0.2.sh
+++ discover/transform_expression_abundance_into_binary_v0.0.2.sh
@@ -6,152 +6,100 @@
 # Version    : v0.0.1
 # License    : MIT
 
-#SBATCH --time=5:59:0
+#SBATCH --time=2:59:0
 #SBATCH --output=%j-%u-transform_expression_abundance_into_binary_v0.0.2.log
-#SBATCH --job-name=teaibs
+#SBATCH --job-name=transform_expression_abundance_into_binary
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=5
-#SBATCH --mem=55G
+#SBATCH --mem=25G
 
-module load R/3.3.3-foss-2015b
+set -o errexit
+set -o errtrace
 
-check_dir(){
-    [ -d $1 ] && echo "Find $1" || exit 1
+DEBUG=0
+
+OPEN_LOG() {
+	exec $1>$2
 }
 
-WKHM=/home/umcg-zzhang/Documents/projects/ASEPrediction/training
+CLOSE_LOG() {
+	exec $1>&-
+}
+
+ERROR() {
+	echo -e "ERROR: $1" >&2 && exit -1
+}
+
+INFOR() {
+	echo -e "INFOR: $1" >&2
+}
+
+check_dir() {
+	[ -d $1 ] && INFOR "Found dir ${1}" || ERROR "NOT found dir ${1}"
+}
+
+check_file() {
+	[ -f $1 ] && INFOR "Found file ${1}" || ERROR "NOT found file ${1}"
+}
+
+check_success() {
+	cmd=${2:-Last command}
+	[ $1 -eq 0 ] && INFOR "${cmd} success!" || ERROR "${cmd} non-zero exit ..."
+}
+
+mkdir_p() {
+	mkdir -p $@
+	check_success $? "mkdir -p $@"
+}
+
+
+module purge
+module load R/3.3.3-foss-2015b-bare
+module list
+
+WKHM=${HOME}/Documents/projects/ASEPrediction/validating
 exeDir=${WKHM}/scripts
-dstDir=${WKHM}/outputs/annotCadd
+dstDir=${WKHM}/outputs/cadd_annotation
 scpName=transform_expression_abundance_into_binary_v0.0.2.R
 
 errDir=${dstDir}/errs
 outDir=${dstDir}/outs
-mkdir ${outDir} ${errDir}
+mkdir_p ${outDir} ${errDir}
 
 check_dir ${exeDir}
 check_dir ${dstDir}
 check_dir ${errDir}
 check_dir ${outDir}
+check_file ${exeDir}/${scpName}
 
-srun -n 1 -c 1 --mem=30G \
-	--error ${errDir}/chr1.err \
-	--output ${outDir}/chr1.out \
-	Rscript ${exeDir}/${scpName} ${dstDir}/chr1_training_set.tsv ${dstDir}/chr1.tsv &
-sleep 1m
-srun -n 1 -c 1 --mem=15G \
-	--error ${errDir}/chr2.err \
-	--output ${outDir}/chr2.out \
-	Rscript ${exeDir}/${scpName} ${dstDir}/chr2_training_set.tsv ${dstDir}/chr2.tsv &
-wait
+start_chr=22
+stop_chr=22
 
-srun -n 1 -c 1 --mem=10G \
-	--error ${errDir}/chr3.err \
-	--output ${outDir}/chr3.out \
-	Rscript ${exeDir}/${scpName} ${dstDir}/chr3_training_set.tsv ${dstDir}/chr3.tsv &
-sleep 1m
-srun -n 1 -c 1 --mem=8G \
-	--error ${errDir}/chr4.err \
-	--output ${outDir}/chr4.out \
-	Rscript ${exeDir}/${scpName} ${dstDir}/chr4_training_set.tsv ${dstDir}/chr4.tsv &
-sleep 1m
-srun -n 1 -c 1 --mem=10G \
-	--error ${errDir}/chr5.err \
-	--output ${outDir}/chr5.out \
-	Rscript ${exeDir}/${scpName} ${dstDir}/chr5_training_set.tsv ${dstDir}/chr5.tsv &
-sleep 1m
-srun -n 1 -c 1 --mem=10G \
-	--error ${errDir}/chr6.err \
-	--output ${outDir}/chr6.out \
-	Rscript ${exeDir}/${scpName} ${dstDir}/chr6_training_set.tsv ${dstDir}/chr6.tsv &
-sleep 1m
-srun -n 1 -c 1 --mem=12G \
-	--error ${errDir}/chr7.err \
-	--output ${outDir}/chr7.out \
-	Rscript ${exeDir}/${scpName} ${dstDir}/chr7_training_set.tsv ${dstDir}/chr7.tsv &
-wait
+for chr in {22..22}; do
+	input_file=${dstDir}/chr${chr}_set.tsv
+	output_file=${dstDir}/chr${chr}_training_set.tsv
+	INFOR "Processing ${input_file} ${output_file}"
+	srun -n 1 -c 1 --mem=5G \
+		--error ${errDir}/chr${chr}.err \
+		--output ${outDir}/chr${chr}.out \
+		Rscript ${exeDir}/${scpName} ${input_file} ${output_file} &
 
-srun -n 1 -c 1 --mem=10G \
-	--error ${errDir}/chr8.err \
-	--output ${outDir}/chr8.out \
-	Rscript ${exeDir}/${scpName} ${dstDir}/chr8_training_set.tsv ${dstDir}/chr8.tsv &
-sleep 1m
-srun -n 1 -c 1 --mem=10G \
-	--error ${errDir}/chr9.err \
-	--output ${outDir}/chr9.out \
-	Rscript ${exeDir}/${scpName} ${dstDir}/chr9_training_set.tsv ${dstDir}/chr9.tsv &
-sleep 1m
-srun -n 1 -c 1 --mem=10G \
-	--error ${errDir}/chr10.err \
-	--output ${outDir}/chr10.out \
-	Rscript ${exeDir}/${scpName} ${dstDir}/chr10_training_set.tsv ${dstDir}/chr10.tsv &
-sleep 1m
-srun -n 1 -c 1 --mem=10G \
-	--error ${errDir}/chr11.err \
-	--output ${outDir}/chr11.out \
-	Rscript ${exeDir}/${scpName} ${dstDir}/chr11_training_set.tsv ${dstDir}/chr11.tsv &
-sleep 1m
-srun -n 1 -c 1 --mem=10G \
-	--error ${errDir}/chr12.err \
-	--output ${outDir}/chr12.out \
-	Rscript ${exeDir}/${scpName} ${dstDir}/chr12_training_set.tsv ${dstDir}/chr12.tsv &
-wait
+	INFOR "Created slave worker for chr${chr}..."
+	sleep 1m
+	[ $[ ${chr} % 4 ] -eq 0 ] && INFOR "Waiting..." && wait
 
-srun -n 1 -c 1 --mem=3G \
-	--error ${errDir}/chr13.err \
-	--output ${outDir}/chr13.out \
-	Rscript ${exeDir}/${scpName} ${dstDir}/chr13_training_set.tsv ${dstDir}/chr13.tsv &
-sleep 1m
-srun -n 1 -c 1 --mem=10G \
-	--error ${errDir}/chr14.err \
-	--output ${outDir}/chr14.out \
-	Rscript ${exeDir}/${scpName} ${dstDir}/chr14_training_set.tsv ${dstDir}/chr14.tsv &
-sleep 1m
-srun -n 1 -c 1 --mem=10G \
-	--error ${errDir}/chr15.err \
-	--output ${outDir}/chr15.out \
-	Rscript ${exeDir}/${scpName} ${dstDir}/chr15_training_set.tsv ${dstDir}/chr15.tsv &
-sleep 1m
-srun -n 1 -c 1 --mem=12G \
-	--error ${errDir}/chr16.err \
-	--output ${outDir}/chr16.out \
-	Rscript ${exeDir}/${scpName} ${dstDir}/chr16_training_set.tsv ${dstDir}/chr16.tsv &
-sleep 1m
-srun -n 1 -c 1 --mem=15G \
-	--error ${errDir}/chr17.err \
-	--output ${outDir}/chr17.out \
-	Rscript ${exeDir}/${scpName} ${dstDir}/chr17_training_set.tsv ${dstDir}/chr17.tsv &
-wait
-
-srun -n 1 -c 1 --mem=10G \
-	--error ${errDir}/chr18.err \
-	--output ${outDir}/chr18.out \
-	Rscript ${exeDir}/${scpName} ${dstDir}/chr18_training_set.tsv ${dstDir}/chr18.tsv &
-sleep 1m
-srun -n 1 -c 1 --mem=15G \
-	--error ${errDir}/chr19.err \
-	--output ${outDir}/chr19.out \
-	Rscript ${exeDir}/${scpName} ${dstDir}/chr19_training_set.tsv ${dstDir}/chr19.tsv &
-sleep 1m
-srun -n 1 -c 1 --mem=10G \
-	--error ${errDir}/chr20.err \
-	--output ${outDir}/chr20.out \
-	Rscript ${exeDir}/${scpName} ${dstDir}/chr20_training_set.tsv ${dstDir}/chr20.tsv &
-sleep 1m
-srun -n 1 -c 1 --mem=5G \
-	--error ${errDir}/chr21.err \
-	--output ${outDir}/chr21.out \
-	Rscript ${exeDir}/${scpName} ${dstDir}/chr21_training_set.tsv ${dstDir}/chr21.tsv &
-sleep 1m
-srun -n 1 -c 1 --mem=10G \
-	--error ${errDir}/chr22.err \
-	--output ${outDir}/chr22.out \
-	Rscript ${exeDir}/${scpName} ${dstDir}/chr22_training_set.tsv ${dstDir}/chr22.tsv &
-wait
-
-
-head -1 ${dstDir}/chr1.tsv | tr "." "-" > ${dstDir}/training_set.tsv
-for fl in ${dstDir}/chr{1..22}.tsv; do
-	sed -n '2,$p' ${fl} >> ${dstDir}/trainig_set.tsv
+	[ ${DEBUG} -eq 1 ] && break
 done
 
-rm ${dstDir}/chr{1..22}.tsv ${dstDir}/chr{1..22}_training_set.tsv -fr
+INFOR "Waiting for the rest workers ..." && wait
+check_success $? "for loop"
+
+head -1 ${dstDir}/chr1_training_set.tsv | tr "." "-" > ${dstDir}/training_set.tsv
+for fl in ${dstDir}/chr{1..22}_training_set.tsv; do
+	sed -n '2,$p' ${fl} >> ${dstDir}/training_set.tsv
+  [ ${DEBUG} -eq 1 ] && break
+done
+check_success $?
+
+rm ${dstDir}/chr{1..22}_set.tsv ${dstDir}/chr{1..22}_training_set.tsv -fr
+check_success $?
