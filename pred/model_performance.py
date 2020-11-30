12e57a71e0e874d6e4fd2bb138269e735408dea2
zhenhua-zhang <zhenhua.zhang217@gmail.com>
2020-11-30 16:32:41 +0100

    fix filename typos

--- pred/model_performance.py
+++ pred/model_performance.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 '''Evaluate model performance by comparing quant and pred results.
+
+NOTE: The input file is typlically the output from scripts/plot/manhattan_plot.py.
+
+TODO:
+    Plan to modify the script to use predicted results instead. If so, have to
+    refer script scripts/plot/manhattan_plot.py
 '''
 
 import argparse
-import gzip
 
 import numpy as np
 import pandas as pd
-
 import sklearn.metrics as smt
 
 
@@ -16,20 +20,13 @@ def getargs():
     '''Get CLI arguments.
     '''
     parser = argparse.ArgumentParser(description="Evaluate model performance by comparing quantification and prediction results.")
-    parser.add_argument('-i', '--input-file', dest='input_file', required=True, help='The input files.')
+    parser.add_argument('-i', '--input-file', dest='input_file', required=True, help='The input files that is typlically from script scripts/plot/manhattan_plot.py')
     parser.add_argument('-o', '--output-pref', dest='output_pref', default='./model_compare', help='The output prefix. Default: %(default)s')
-
     return parser
 
 
-def cmp_quan_pred():
-    '''Compare quantification and prediction results.
-    '''
-    pass
-
-
-
 class EvalMatrix:
+    '''A class to print confusion matrix and its expansion.'''
     def __init__(self, para_dict):
         for key, val in para_dict.items():
             setattr(self, key, val)
@@ -63,8 +60,13 @@ class EvalMatrix:
        False discovery rate: {fdr:.{prec}f}'''.format(s='', **self.para_dict)
         return report_str
 
+    def show(self):
+        '''Show the confusion matrix.'''
+        print(self.__str__())
+
 
 def acc_ci(mean, err, size, const=1.96):
+    '''Accuracy confident interval.'''
     itvl = const * np.sqrt((err * (1 - err)) / size)
     return mean - itvl, mean + itvl
 
@@ -100,13 +102,12 @@ def calc_eval_matrix(dtfm, cond, y_true, y_pred=None, y_score=None,
 
     ps_lable, ng_lable = class_label
     width = max(len(ps_lable), len(ng_lable))
-    conf_dict = {'title': title, 'ps_lable': ps_lable, 'ng_label': ng_lable,
-                 'tnc': tnc, 'fpc': fpc, 'fnc': fnc, 'tpc': tpc, 'tpr': tpr,
-                 'tnr': tnr, 'ppv': ppv, 'npv': npv, 'fdr': fdr, 'nif': nif,
-                 'acc': acc, 'acc_lower': acc_lower, 'acc_upper': acc_upper,
-                 'roc_auc': roc_auc, 'prec': prec, 'width': width}
-
-    return EvalMatrix(conf_dict)
+    return EvalMatrix({
+        'title': title, 'ps_lable': ps_lable, 'ng_label': ng_lable,
+        'tnc': tnc, 'fpc': fpc, 'fnc': fnc, 'tpc': tpc, 'tpr': tpr,
+        'tnr': tnr, 'ppv': ppv, 'npv': npv, 'fdr': fdr, 'nif': nif,
+        'acc': acc, 'acc_lower': acc_lower, 'acc_upper': acc_upper,
+        'roc_auc': roc_auc, 'prec': prec, 'width': width})
 
 
 def main():
@@ -120,36 +121,48 @@ def main():
     report_path = '{}-performance_report.txt'.format(output_pref)
     with open(report_path, 'w') as opfh:
         # Crosstab of SNVs that are shared by both cohort, apply BIOS model on GTEx cohort
-        snvs_both = dtfm['bb_ASE_gtex'].notna() & dtfm['bb_ASE_bios'].notna()
-        em = calc_eval_matrix(dtfm, snvs_both, y_true='bb_ASE_gtex',
-                              y_score='prob1_mean_gtex',
-                              title='Report 1: Shared SNVs, BIOS model on GTEx dataset')
-        opfh.write(str(em))
+        snvs_gtex = dtfm['bb_ASE_gtex'].notna()
+        cem = calc_eval_matrix(dtfm, snvs_gtex, y_true='bb_ASE_gtex',
+                               y_score='prob1_mean_gtex',
+                               title='Report 1: GTEx SNVs, BIOS model on GTEx dataset')
+        opfh.write(str(cem))
 
-        em = calc_eval_matrix(dtfm, snvs_both, y_true='bb_ASE_bios',
-                              y_score='prob1_mean_bios',
-                              title='Report 2: Shared SNVs, GTEx model on BIOS dataset')
-        opfh.write(str(em))
+        snvs_bios = dtfm['bb_ASE_bios'].notna()
+        cem = calc_eval_matrix(dtfm, snvs_bios, y_true='bb_ASE_bios',
+                               y_score='prob1_mean_bios',
+                               title='Report 2: BIOS SNVs, GTEx model on BIOS dataset')
+        opfh.write(str(cem))
+
+        snvs_both = dtfm['bb_ASE_gtex'].notna() & dtfm['bb_ASE_bios'].notna()
+        cem = calc_eval_matrix(dtfm, snvs_both, y_true='bb_ASE_gtex',
+                               y_score='prob1_mean_gtex',
+                               title='Report 3: Shared SNVs, BIOS model on GTEx dataset')
+        opfh.write(str(cem))
+
+        cem = calc_eval_matrix(dtfm, snvs_both, y_true='bb_ASE_bios',
+                               y_score='prob1_mean_bios',
+                               title='Report 4: Shared SNVs, GTEx model on BIOS dataset')
+        opfh.write(str(cem))
 
         # Crosstab of SNVs that only exists in GTEx cohort, apply BIOS model on GTEx cohort.
-        snvs_gtex = dtfm['bb_ASE_gtex'].notna() & dtfm['bb_ASE_bios'].isna()
-        em = calc_eval_matrix(dtfm, snvs_gtex, y_true='bb_ASE_gtex',
-                              y_score='prob1_mean_gtex',
-                              title='Report 3: GTEx only SNVs, BIOS model on GTEx dataset')
-        opfh.write(str(em))
+        snvs_gtex_only = dtfm['bb_ASE_gtex'].notna() & dtfm['bb_ASE_bios'].isna()
+        cem = calc_eval_matrix(dtfm, snvs_gtex_only, y_true='bb_ASE_gtex',
+                               y_score='prob1_mean_gtex',
+                               title='Report 5: GTEx only SNVs, BIOS model on GTEx dataset')
+        opfh.write(str(cem))
 
         # Crosstab of SNVs that only exists in BIOS cohort, apply GTEx model on BIOS cohort.
-        snvs_bios = dtfm['bb_ASE_gtex'].isna() & dtfm['bb_ASE_bios'].notna()
-        em = calc_eval_matrix(dtfm, snvs_bios, y_true='bb_ASE_bios',
-                              y_score='prob1_mean_bios',
-                              title='Report 4: BIOS only SNVs, GTEx model on BIOS dataset')
-        opfh.write(str(em))
+        snvs_bios_only = dtfm['bb_ASE_gtex'].isna() & dtfm['bb_ASE_bios'].notna()
+        cem = calc_eval_matrix(dtfm, snvs_bios_only, y_true='bb_ASE_bios',
+                               y_score='prob1_mean_bios',
+                               title='Report 6: BIOS only SNVs, GTEx model on BIOS dataset')
+        opfh.write(str(cem))
 
     gtex_only_opt = '{}-gtex_only_snps.csv'.format(output_pref)
-    dtfm.loc[snvs_gtex, :].to_csv(gtex_only_opt, index=False)
+    dtfm.loc[snvs_gtex_only, :].to_csv(gtex_only_opt, index=False)
 
     bios_only_opt = '{}-bios_only_snps.csv'.format(output_pref)
-    dtfm.loc[snvs_bios, :].to_csv(bios_only_opt, index=False)
+    dtfm.loc[snvs_bios_only, :].to_csv(bios_only_opt, index=False)
 
 if __name__ == '__main__':
     main()
