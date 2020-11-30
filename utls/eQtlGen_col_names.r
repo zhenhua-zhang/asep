2950fd73c8be5198ef164c6ad19cc901887592e2
zhenhua-zhang <zhenhua.zhang217@gmail.com>
2020-11-30 22:53:12 +0100

    arrange misc/ and utls/ folders

--- utls/eQtlGen_col_names.r
+++
@@ -1,6 +0,0 @@
-col_names <- c(
-    "Pvalue",         "SNP",            "SNPChr",         "SNPPos",
-    "AssessedAllele", "OtherAllele",    "Zscore",         "Gene",
-    "GeneSymbol",     "GeneChr",        "GenePos",        "NrCohorts",
-    "NrSamples",      "FDR"
-)
